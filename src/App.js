e5c1fb81f7c5d90d780b75d20d7b73a1b122aa37
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-16 18:30:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState();
+
+    const getElements = async () => {
+        const response = await fetch("http://localhost:8080/api/products")
+            .then((data) => data.json())
+            .then((data) => {
+                setProducts(data);
+                console.log("Successful GET");
+            });
+    };
+
+    console.log(products);
+
+    useEffect(() => {
+        getElements();
+    }, []);
+    return (
+        <div className="App">
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td>Amount</td>
+                    <td>Include in Invoice</td>
+                </tr>
+                {products && products.map((product) => (
+                        <tr key={product.id}>
+                            <td style={{border: '1px solid black'}}>{product.name}</td>
+                            <td style={{border: '1px solid black'}}>{product.amount}</td>
+                            <td style={{border: '1px solid black'}}><input type="checkbox"/></td>
+                        </tr>
+                        ))}
+            </table>
+            {products ? (
+                    <button>Generate Invoice from Selected</button>
+            ) : ( <div/>)}
+        </div>
+    );
 }
 
 export default App;
