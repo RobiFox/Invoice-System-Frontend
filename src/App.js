742b29543deea9465c2178af1324e6462b266211
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-17 13:28:09 +0100

    added a way to pick selected ids

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import {useState, useEffect} from "react";
 
 function App() {
-    const [products, setProducts] = useState();
+    const [products, setProducts] = useState([]);
     const [loadingStatus, setLoadingStatus] = useState();
+
+    function onInvoiceGenerateClick() {
+        console.log("Hi");
+        const selectedIds = products.filter(value => document.getElementById("checkbox-" + value.id).checked)
+            .map(value => {
+                return value.id;
+            });
+        console.log(selectedIds);
+    }
 
     const getElements = async () => {
         const response = await fetch("http://localhost:8080/api/products")
@@ -13,10 +22,10 @@ function App() {
                 setProducts(data);
                 console.log("Successful GET");
             })
-        .catch(reason => {
-            setLoadingStatus(reason.toString());
-            console.log("Fetch error: " + reason);
-        });
+            .catch(reason => {
+                setLoadingStatus(reason.toString());
+                console.log("Fetch error: " + reason);
+            });
     };
 
     console.log(products);
@@ -38,15 +47,16 @@ function App() {
                     <p>{loadingStatus}</p>
                 )}
                 {products && products.map((product) => (
-                    <tr key={product.id}>
+                    <tr key={"row-" + product.id}>
                         <td style={{border: '1px solid black'}}>{product.name}</td>
                         <td style={{border: '1px solid black'}}>{product.amount}</td>
-                        <td style={{border: '1px solid black'}}><input type="checkbox"/></td>
+                        <td style={{border: '1px solid black'}}><input type="checkbox" id={"checkbox-" + product.id}/>
+                        </td>
                     </tr>
                 ))}
             </table>
             {products ? (
-                <button>Generate Invoice from Selected</button>
+                <button onClick={onInvoiceGenerateClick}>Generate Invoice from Selected</button>
             ) : (<div/>)}
         </div>
     );
